2434778566dcae9ee960aeb1ea21ce9952ce8188
biyakim <w2116@e-mirim.hs.kr>
2022-12-08 21:47:00 +0900

    import as 문ㅇ르 사용하여 모듈 일믕르 cal로 변경

---
+++ 실습/calmain4.py
@@ -0,0 +1,10 @@
+import calculator as cal
+
+
+n1 = int(input("첫 번째 수는? : "))
+n2 = int(input("두 번째 수는? :"))
+
+print(n1,"+",n2,"=",cal.add(n1,n2))
+print(n1,"-",n2,"=",cal.sub(n1,n2))
+print(n1,"*",n2,"=",cal.mul(n1,n2))
+print(n1,"/",n2,"=",cal.div(n1,n2))
