8135889d796282c9b1868d3bd13072939941df94
biyakim <w2116@e-mirim.hs.kr>
2022-12-08 17:35:50 +0900

    구구단 예제2

---
+++ 실습/gugudan3.py
@@ -0,0 +1,9 @@
+def gugudan3(num1,num2,num3):
+    for i in range(1,10):
+        print(num1,"x",i,"=",num1*i)
+    for i in range(1,10):
+        print(num2,"x",i,"=",num2*i)
+    for i in range(1,10):
+        print(num3,"x",i,"=",num3*i)
+
+gugudan3(2,3,7)
